065025b98c2b5c0b39b2bae4a52d32214bca4e15
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2016-05-15 02:01:25 +0900

    refactoring

--- match.go
+++ match.go
@@ -1,7 +1,6 @@
 package fuzzy
 
 import (
-	"fmt"
 	"unicode"
 )
 
@@ -12,21 +11,25 @@ func max(a, b int) int {
 	return a
 }
 
-func Match(pattern, str string) (bool, int, string) {
+func Match(pattern, str string, formattedStr *string) (bool, int) {
 	// Score consts
-	var adjacency_bonus = 5             // bonus for adjacent matches
-	var separator_bonus = 10            // bonus if match occurs after a separator
-	var camel_bonus = 10                // bonus if match is uppercase and prev is lower
-	var leading_letter_penalty = -3     // penalty applied for every letter in str before the first match
-	var max_leading_letter_penalty = -9 // maximum penalty for leading letters
-	var unmatched_letter_penalty = -1   // penalty for every letter that doesn't matter
+	const (
+		adjacencyBonus          = 5  // bonus for adjacent matches
+		separatorBonus          = 10 // bonus if match occurs after a separator
+		camelBonus              = 10 // bonus if match is uppercase and prev is lower
+		leadingLetterPenalty    = -3 // penalty applied for every letter in str before the first match
+		maxLeadingLetterPenalty = -9 // maximum penalty for leading letters
+		unmatchedLetterPenalty  = -1 // penalty for every letter that doesn't matter
+	)
 
 	// Loop variables
 	var score = 0
-	var patternIdx = 0
-	var patternLength = len([]rune(pattern))
-	var strIdx = 0
-	var strLength = len([]rune(str))
+	var rp = []rune(pattern)
+	var rs = []rune(str)
+	var ip = 0
+	var lp = len(rp)
+	var is = 0
+	var ls = len(rs)
 	var prevMatched = false
 	var prevLower = false
 	var prevSeparator = true // true so if first letter match gets separator bonus
@@ -36,30 +39,29 @@ func Match(pattern, str string) (bool, int, string) {
 	var bestLower = rune(0)
 	var bestLetterIdx = 0
 	var bestLetterScore = 0
-	var formattedStr = ""
 
 	var matchedIndices []int
 
 	// Loop over strings
-	for strIdx != strLength {
-		var patternChar = rune(0)
-		if patternIdx != patternLength {
-			patternChar = []rune(pattern)[patternIdx]
+	for is < ls {
+		var cp = rune(0)
+		if ip != lp {
+			cp = rp[ip]
 		}
-		var strChar = []rune(str)[strIdx]
+		var cs = rs[is]
 
 		var patternLower = rune(0)
-		if patternChar != 0 {
-			patternLower = unicode.ToLower(patternChar)
+		if cp != 0 {
+			patternLower = unicode.ToLower(cp)
 		}
-		var strLower = unicode.ToLower(strChar)
-		var strUpper = unicode.ToUpper(strChar)
+		var strLower = unicode.ToLower(cs)
+		var strUpper = unicode.ToUpper(cs)
 
-		var nextMatch = patternChar != 0 && patternLower == strLower
+		var nextMatch = cp != 0 && patternLower == strLower
 		var rematch = bestLetter != 0 && bestLower == strLower
 
 		var advanced = nextMatch && bestLetter != 0
-		var patternRepeat = bestLetter != 0 && patternChar != 0 && bestLower == patternLower
+		var patternRepeat = bestLetter != 0 && cp != 0 && bestLower == patternLower
 		if advanced || patternRepeat {
 			score += bestLetterScore
 			matchedIndices = append(matchedIndices, bestLetterIdx)
@@ -74,29 +76,29 @@ func Match(pattern, str string) (bool, int, string) {
 
 			// Apply penalty for each letter before the first pattern match
 			// Note: std::max because penalties are negative values. So max is smallest penalty.
-			if patternIdx == 0 {
-				var penalty = max(strIdx*leading_letter_penalty, max_leading_letter_penalty)
+			if ip == 0 {
+				var penalty = max(is*leadingLetterPenalty, maxLeadingLetterPenalty)
 				score += penalty
 			}
 
 			// Apply bonus for consecutive bonuses
 			if prevMatched {
-				newScore += adjacency_bonus
+				newScore += adjacencyBonus
 			}
 
 			// Apply bonus for matches after a separator
 			if prevSeparator {
-				newScore += separator_bonus
+				newScore += separatorBonus
 			}
 
 			// Apply bonus across camel case boundaries. Includes "clever" isLetter check.
-			if prevLower && strChar == strUpper && strLower != strUpper {
-				newScore += camel_bonus
+			if prevLower && cs == strUpper && strLower != strUpper {
+				newScore += camelBonus
 			}
 
 			// Update patter index IFF the next pattern letter was matched
 			if nextMatch {
-				patternIdx += 1
+				ip += 1
 			}
 
 			// Update best letter in str which may be for a "next" letter or a "rematch"
@@ -104,26 +106,26 @@ func Match(pattern, str string) (bool, int, string) {
 
 				// Apply penalty for now skipped letter
 				if bestLetter != 0 {
-					score += unmatched_letter_penalty
+					score += unmatchedLetterPenalty
 				}
 
-				bestLetter = strChar
+				bestLetter = cs
 				bestLower = unicode.ToLower(bestLetter)
-				bestLetterIdx = strIdx
+				bestLetterIdx = is
 				bestLetterScore = newScore
 			}
 
 			prevMatched = true
 		} else {
-			score += unmatched_letter_penalty
+			score += unmatchedLetterPenalty
 			prevMatched = false
 		}
 
 		// Includes "clever" isLetter check.
-		prevLower = strChar == strLower && strLower != strUpper
-		prevSeparator = strChar == '_' || strChar == ' '
+		prevLower = cs == strLower && strLower != strUpper
+		prevSeparator = cs == '_' || cs == ' '
 
-		strIdx += 1
+		is += 1
 	}
 
 	// Apply score for last match
@@ -132,16 +134,18 @@ func Match(pattern, str string) (bool, int, string) {
 		matchedIndices = append(matchedIndices, bestLetterIdx)
 	}
 
-	// Finish out formatted string after last pattern matched
-	// Build formated string based on matched letters
-	var lastIdx = 0
-	rs := []rune(str)
-	for _, idx := range matchedIndices {
-		formattedStr += string(rs[lastIdx:idx-lastIdx]) + "<b>" + string(rs[idx]) + "</b>"
-		lastIdx = idx + 1
+	if formattedStr != nil {
+		// Finish out formatted string after last pattern matched
+		// Build formated string based on matched letters
+		li := 0
+		s := ""
+		for _, idx := range matchedIndices {
+			s += string(rs[li:idx-li]) + "<b>" + string(rs[idx]) + "</b>"
+			li = idx + 1
+		}
+		s += string(rs[li:])
+		*formattedStr = s
 	}
-	formattedStr += string(rs[lastIdx:])
 
-	var matched = patternIdx == patternLength
-	return matched, score, formattedStr
+	return ip == lp, score
 }
